71a5122fa2c683755e56888fccf19949edb70b8e
cemdrman <cemdrman@gmail.com>
2024-01-29 19:00:49 +0300

    init

---
+++ src/main/java/com/bilisimio/webflux/BilisimioApplication.java
@@ -0,0 +1,13 @@
+package com.bilisimio.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilisimioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilisimioApplication.class, args);
+    }
+
+}
